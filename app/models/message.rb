948ef61d777c3b8ef99e79726d1cd7e691c12207
Kelmetelser[1992] <mahmoudhussien25@gmail.com>
2023-01-08 21:48:08 +0200

    action cable

---
+++ app/models/message.rb
@@ -0,0 +1,11 @@
+class Message < ApplicationRecord
+  after_create_commit { broadcast_message }
+  belongs_to :user
+
+
+  private
+
+  def broadcast_message
+    ActionCable.server.broadcast('MessagesChannel', {id:, body:})
+  end
+end
